828b0a3f2ad7394b958934e6ee5d9fb1d8b6cf51
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-04-17 01:09:02 -0300

    Bulk insert into MongoDB using insertMany.

--- mongodb.js
+++ mongodb.js
@@ -15,5 +15,46 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     db.collection('users').insertOne({
         name: 'Bruno',
         age: 33
+    }, (error, result) => {
+        if(error){
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
+    })
+
+    db.collection('users').insertMany([
+        {
+            name: 'Jen',
+            age: 28
+        },{
+            name: 'Gunther',
+            age: 27
+        }
+    ], (error, result) => {
+        if(error){
+            return console.log('Unable to insert documents!')
+        }
+
+        console.log(result.ops)
+    })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Read the Bible',
+            completed: true
+        },{
+            description: 'Learn Golang',
+            completed: false
+        },{
+            description: 'Teach programming to my children',
+            completed: true
+        }
+    ], (error, result) => {
+        if(error){
+            return console.log('Unable to insert tasks!')
+        }
+
+        console.log(result.ops)
     })
 })
