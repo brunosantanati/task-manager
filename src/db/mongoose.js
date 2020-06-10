66d7dc2b4a9895b06c60c4a8888360f48fdbef7e
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-10 03:02:15 -0300

    Data validation and sanitization.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,31 +1,52 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-/*const User = mongoose.model('User', {
+const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+               throw new Error('Email is invalid') 
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0){
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: 'Bruno',
-    age: 33
+    name: ' Bruno ',
+    email: ' BRUNO@bruno.com      ',
+    //age: 33
 })
 
 me.save().then((me) => {
     console.log(me)
 }).catch((error) => {
     console.log('Error!', error)
-})*/
+})
 
-const Task = mongoose.model('Task', {
+/*const Task = mongoose.model('Task', {
     description: {
         type: String
     },
@@ -43,4 +64,4 @@ task.save().then(() => {
     console.log(task)
 }).catch((error) => {
     console.log(error)
-})
+})*/
