2d6489f52de03d182295b35da9fbbdef1af3e92f
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-11-21 14:50:59 -0300

    Resource creation endpoint (user)

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 <code>node src/db/mongoose.js</code>  
 <code>npm i nodemon@1.18.9 --save-dev</code>  
 <code>npm i express@4.16.4</code>  
+<code>npm run dev</code>  
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,62 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-               throw new Error('Email is invalid') 
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-const me = new User({
-    name: ' Bruno ',
-    email: ' BRUNO@bruno.com      ',
-    password: 'phone098!'
-    //age: 33
-})
-
-/*me.save().then((me) => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})*/
 
 const Task = mongoose.model('Task', {
     description: {
@@ -68,19 +15,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     }
-})
-
-/*const task = new Task({
-    description: 'Learn Kubernetes',
-    completed: false
-})*/
-
-const task = new Task({
-    description: '   Learn Kubernetes  ' //test trim and default value
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
 })
