e6c33bd47ddf4652a1deef56bf955de619ba5b51
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-01-27 12:54:59 -0300

    Use a real ID for a existing user

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,7 +1,7 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5c1a5a34d5a2ec046ca8f6bc', { age: 1 }).then((user) => {
+User.findByIdAndUpdate('637bb65368130a96b9e0a995', { age: 1 }).then((user) => {
     console.log(user)
     return User.countDocuments({ age: 1 })
 }).then((result) => {
