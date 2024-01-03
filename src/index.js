0f3ee61d1a6cd20dc64cad0a3afc7499aa8de32b
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-01-03 02:37:01 -0300

    Accept Authentication Tokens (JWT)

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ const port = process.env.PORT || 3000
   } else {
     next()
   }
-})*/
+})
 
 app.use((req, res, next) => {
   res.status(503).send('Site is currently down. Check back soon!')
-})
+})*/
 
 app.use(express.json())
 app.use(userRouter)
