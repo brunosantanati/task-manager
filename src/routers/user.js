0732b3f3885e44c89cc21f3736dbca4c007dc7f4
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-01-04 20:50:50 -0300

    Logout user

--- src/routers/user.js
+++ src/routers/user.js
@@ -34,6 +34,29 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
 // Modifying this endpoint to return the current logged user instead of all users
 //router.get('/users', auth, async (req, res) => {
 router.get('/users/me', auth, async (req, res) => {
