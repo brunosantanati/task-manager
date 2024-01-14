86e2955b5a97abb22d71d2b91c491b98dc31cdd8
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-01-14 17:16:42 -0300

    Setup authentication for user endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -69,7 +69,8 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
+// the current user must not be able to get another user by id, so commenting this out
+/*router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
     try {
@@ -84,9 +85,11 @@ router.get('/users/:id', async (req, res) => {
         console.log(e)
         res.status(500).send()
     }
-})
+})*/
 
-router.patch('/users/:id', async (req, res) => {
+// change route to modify currenty user instead of modifying any user by id
+// old implementation
+/*router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -112,9 +115,29 @@ router.patch('/users/:id', async (req, res) => {
     } catch(e) {
         res.status(400).send(e)
     }
+})*/
+//new implementation
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch(e) {
+        res.status(400).send(e)
+    }
 })
 
-router.delete('/users/:id', async (req, res) => {
+// change route to delete current user instead of deleting user by id
+//old implementation
+/*router.delete('/users/:id', auth, async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id)
 
@@ -123,6 +146,15 @@ router.delete('/users/:id', async (req, res) => {
         }
 
         res.send(user)
+    } catch(e) {
+        res.status(500).send()
+    }
+})*/
+//new implementation
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        await req.user.remove()
+        res.send(req.user)
     } catch(e) {
         res.status(500).send()
     }
