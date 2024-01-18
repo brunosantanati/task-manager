cffad5bc4402681c9d264de3ce69362a3e0b5e38
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-01-18 02:37:03 -0300

    Add endpoint to upload avatar (profile pic)

--- src/index.js
+++ src/index.js
@@ -21,13 +21,13 @@ app.use((req, res, next) => {
 })*/
 
 // ############### Multer test/example
-const multer = require('multer')
+/*const multer = require('multer')
 const upload = multer({
   dest: 'images'
 })
 app.post('/upload', upload.single('upload'), (req, resp) => {
   resp.send()
-})
+})*/
 
 app.use(express.json())
 app.use(userRouter)
