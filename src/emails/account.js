636302184301b5c60d26177c914dc7ab89cec03b
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-01-21 02:13:25 -0300

    Use env var to hold SendGrid API Key

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = 'SG.1OuT9F6zRpONvtPtBO79MQ.f39LMSFPaDHtolIzON97Rmr9nMMFHvQ569EqHH1B3Ms'
+const sendgridAPIKey = process.env.SG_MAIL_KEY
 
 sgMail.setApiKey(sendgridAPIKey)
 
