819d88cf9e3f30b24f0af3bf773405f171ccff08
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-01-22 22:50:02 -0300

    Send welcome and cancelation emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,25 @@ const sendgridAPIKey = process.env.SG_MAIL_KEY
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to: 'bruno.santana.ti@gmail.com',
-    from: 'bruno.santana.ti@gmail.com',
-    subject: 'This is my first creation!',
-    text: 'I hope this one actually get to you.'
-})
+const sendWelcomeEmail = (email, name) =>{
+    sgMail.send({
+        to: email,
+        from: 'bruno.santana.ti@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelationEmail = (email, name) =>{
+    sgMail.send({
+        to: email,
+        from: 'bruno.santana.ti@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const multer = require('multer')
 const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -19,6 +20,7 @@ router.post('/users', async (req, res) => {
     // rewrite the code above using async/await
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -156,6 +158,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendCancelationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch(e) {
         res.status(500).send()
